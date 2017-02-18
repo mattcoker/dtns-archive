e864226498a219074826c52d1b3537740d1fcfc1
Matthew Coker <matt.james.coker@gmail.com>
2017-02-18 06:30:53 -0600

    Add search forms to picks and topics pages

--- app/pods/picks/index/controller.js
+++ app/pods/picks/index/controller.js
@@ -2,5 +2,11 @@ import Ember from 'ember';
 import { storageFor } from 'ember-local-storage';
 
 export default Ember.Controller.extend({
-  settings: storageFor('settings')
+  settings: storageFor('settings'),
+
+  actions: {
+    updateFilterField(arg) {
+      this.set('filterField', arg);
+    },
+  }
 });
