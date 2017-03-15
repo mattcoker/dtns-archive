ceac4a8370895c9918210f4748869015229f58df
Matthew Coker <matt.james.coker@gmail.com>
2017-03-15 13:20:53 -0500

    Add web-app capabilities to DTNS archive

---
+++ config/manifest.js
@@ -0,0 +1,19 @@
+/*jshint node:true*/
+'use strict';
+
+module.exports = function(/* environment, appConfig */) {
+  // See https://github.com/san650/ember-web-app#documentation for a list of
+  // supported properties
+
+  return {
+    name: "DTNS Archive",
+    short_name: "DTNS Archive",
+    description: "Your Dedicated Repository for DTNS episodes, people, picks, and topics.",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#4aa94a",
+    theme_color: "#4aa94a",
+    icons: [
+    ]
+  };
+}
