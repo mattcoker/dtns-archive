d55337435b5bef81fe96d081c36dec02d18006bb
Matthew Coker <matt.james.coker@gmail.com>
2017-03-23 22:23:19 -0500

    Add various logo sizes to application, update config manifest for web-app functionality

--- config/manifest.js
+++ config/manifest.js
@@ -14,6 +14,26 @@ module.exports = function(/* environment, appConfig */) {
     background_color: "#000",
     theme_color: "#000",
     icons: [
+      {
+        src: "/assets/images/logo-192x192.png",
+        sizes: "192x192",
+        type: "image/png"
+      },
+      {
+        src: "/assets/images/logo-384x384.png",
+        sizes: "384x384",
+        type: "image/png"
+      },
+      {
+        src: "/assets/images/logo-768x768.png",
+        sizes: "768x768",
+        type: "image/png"
+      },
+      {
+        src: "/assets/images/logo-1128x1128.png",
+        sizes: "1128x1128",
+        type: "image/png"
+      }
     ]
   };
 }
